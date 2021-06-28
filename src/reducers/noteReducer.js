f3b222cf07caaa51a1ac085c8a74ee3d5f39692a
George Papagapitos <george.g.papagapitos@gmail.com>
2021-06-28 12:46:57 -0500

    refactor reducers

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -16,4 +16,26 @@ const noteReducer = (state = [], action) => {
   }
 }
 
+const generateId = () => {
+  Math.floor(Math.random() * 100000);
+}
+
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
+  }
+}
+
 export default noteReducer;
