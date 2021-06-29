e78e52762fb88a33b87b5b5543ba994d123bd5af
George Papagapitos <george.g.papagapitos@gmail.com>
2021-06-29 11:56:12 -0500

    added mock-db

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AddNoteForm from './components/AddNoteForm';
 import Notes from './components/Notes';
 import VisibilityFilter from './components/VisibilityFilter';
+import { getAll } from './services/notes';
+import { initializeNotes } from './reducers/noteReducer';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getAll()
+      .then(notes => dispatch(initializeNotes(notes)));
+  }, [dispatch]);
   return (
     <div>
       <AddNoteForm />
--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,21 +1,9 @@
-const initialState =
-  [
-    {
-      content: 'reducer defines how redux store works',
-      important: true,
-      id: 1,
-    },
-    {
-      content: 'state of store can contain any data',
-      important: false,
-      id: 2,
-    },
-  ];
-
-const noteReducer = (state = initialState, action) => {
+const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE':
       return [...state, action.payload];
+    case 'INIT_NOTES':
+      return action.payload;
     case 'TOGGLE_IMPORTANCE': {
       const id = action.payload.id;
       const noteToChange = state.find(n => n.id === id);
@@ -30,18 +18,17 @@ const noteReducer = (state = initialState, action) => {
   }
 }
 
-const generateId = () => {
-  return Math.floor(Math.random() * 100000);
-}
-
-export const createNote = (content) => {
+export const createNote = (data) => {
   return {
     type: 'NEW_NOTE',
-    payload: {
-      content,
-      important: false,
-      id: generateId()
-    }
+    data
+  }
+}
+
+export const initializeNotes = (notes) => {
+  return {
+    type: 'INIT_NOTES',
+    payload: notes
   }
 }
 
